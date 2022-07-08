df1585297a8ecdd499f06e5285449e02507fa06f
AxelGalaxyDev <105892794+AxelGalaxyDev@users.noreply.github.com>
2022-07-07 21:43:37 -0500

    YA

---
+++ .js/dinamico.js
@@ -0,0 +1,16 @@
+
+function Miura() {
+    let caja_Miura = document.getElementById(caja)
+    caja_Miura = "images/miura" ;
+    
+    
+    
+    }
+    
+    const btn_miura = document.querySelectorAll("button")[1]
+    btn_miura.addEventListener("click", Miura)
+    
+    
+    
+    
+    
