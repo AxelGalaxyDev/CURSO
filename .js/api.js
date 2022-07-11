17cf5369a29325d56f9f37a3fa9d3bd428155df6
AxelGalaxyDev <105892794+AxelGalaxyDev@users.noreply.github.com>
2022-07-10 21:19:22 -0500

    10/julio/2022

---
+++ .js/api.js
@@ -0,0 +1,25 @@
+
+const uri="https://raw.githubusercontent.com/AxelGalaxyDev/API/main/";
+
+// ENCONTRAR ELEMENTOS DEL HTML//
+const el_lista = document.getElementById("lista-lambo");
+const el_caja = document.getElementById("caja");
+const el_titulo = document.querySelector("div#lambo-info h2");
+
+fetch(uri + "datos.json").then(function(respuesta) {
+    return respuesta.json();
+}).then(function(datos) {
+  for(let i = 0; i < datos.length; i+=1) {
+   //CREAR NUEVO BOTON//
+   let nuevo_boton = document.createElement("button");
+    nuevo_boton.innerHTML += datos[i].nombre;
+    el_lista.appendChild(nuevo_boton);
+      //AGREGAR EL EVENTO DE CLICK AL BOTON//
+    nuevo_boton.addEventListener("click", function() {
+      el_caja.src = uri + datos[i].imagen;
+     el_titulo.innerHTML =  datos[i].nombre;
+    });
+   }
+}).catch(function(error) {
+    console.log(error);
+});
